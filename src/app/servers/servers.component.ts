a7b2d31d7b93d7db55a746474a9f7a977d078621
Bideo Wego <bideowego@gmail.com>
2019-04-05 21:11:18 -0400

    Add success and warning alert components

--- src/app/servers/servers.component.ts
+++ src/app/servers/servers.component.ts
@@ -1,7 +1,12 @@
 import { Component, OnInit } from '@angular/core';
 
 @Component({
+  // Can select by element
   selector: 'app-servers',
+  // Can select by attribute
+  // selector: '[app-servers]',
+  // Can select by class
+  // selector: '.app-servers',
   templateUrl: './servers.component.html',
   styleUrls: ['./servers.component.css']
 })
